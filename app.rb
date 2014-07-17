55070061adc39750ad667eaec94e6f19d49e95ee
Senthil Nayagam <senthil@railsfactory.com>
2014-07-17 15:35:49 +0530

    implemented a simple session demonstration for username, login and logout

--- app.rb
+++ app.rb
@@ -22,3 +22,24 @@ get '/erb_example' do
   @names = ["Pavani","Usha","Surekha","Sujitha","Manjusagar","Veeresh","Deepanker","Pavankumar","Jagadeesh"].shuffle
   erb :example
 end  
+
+
+
+get '/login' do
+
+erb :login
+end
+
+post '/login' do
+  session[:username]= params[:username]
+  erb :login
+end
+
+
+get '/logout' do
+  session.clear
+  @message= "you have been logged out"
+  erb :logout
+end  
+
+
